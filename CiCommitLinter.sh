af332c2c393296dfa3354bfd5f9c7e6e459f6cc6
Mohammed <technocraft.devops@gmail.com>
2022-01-15 21:17:29 +0100

    test(test): fixing for test

--- CiCommitLinter.sh
+++ CiCommitLinter.sh
@@ -8,6 +8,7 @@ CONVCOMMIT_LIST=$(git log -s --format=%s master..$1  | grep -U --color=never -P 
 INCONVCOMMIT_LIST=$(git log -s --format=%s master..$1 );
 
 DIFF=$(diff  <(echo -e "$INCONVCOMMIT_LIST") <(echo -e "$CONVCOMMIT_LIST"));
+DIFF_COUNT= $(diff  <(echo -e "$INCONVCOMMIT_LIST") <(echo -e "$CONVCOMMIT_LIST") | wc -l);
 
 # Styles
 red=`tput setaf 1`;
@@ -21,26 +22,27 @@ bold=$(tput bold);
 underline=`(tput sgr 0 1)`;
 
 reset=`tput sgr0`;
+echo ${DIFF_COUNT} 
+echo ${DIFF} 
 
-if [ ${DIFF} -ge 1 ];then
-  echo ${underline}${red}  "${bold}not conventionnal commit found" ${reset};
-  echo ${yellow} "-------------" ${reset};
-  echo ${red}"$DIFF" ${reset};
-  echo ${yellow} "-------------" ${reset};
-    echo ${red} "please try => ${yellow}/!\one by one commit/!\\"${rest};
-    echo ${red} "1- ${bold}${whiteBg}git rebase -i HEAD~${INCONVCOMMIT_COUNT}"${reset};
-    echo ${red} "2- ${bold}${whiteBg}replace pick by reward on the commit, save & quit" ${reset};
-    echo ${red} "3- ${bold}${whiteBg}fix the commit message with convientionnal format, save & quit " ${reset};
-    echo ${red} "${bold}${whiteBg}Et voila !" ${reset};
-
-  echo "";
-  echo ${red} "${bold}<-- don't be afraid of rebase we believe in you -->";
-  exit 1;
-else
-  echo ${green} ${bold} "all commit are conventionnals" ${reset};
-  echo ${blue} "-------------" ${reset};
-  echo ${green} "$CONVCOMMIT_LIST" ${reset};
-  echo ${blue} "-------------" ${reset};
-  echo ${bold} ${blue} "good job !" ${reset}
-  exit 0;
-fi
+# if [ ${DIFF_COUNT} -ge 1 ];then
+#   echo ${underline}${red}  "${bold}not conventionnal commit found" ${reset};
+#   echo ${yellow} "-------------" ${reset};
+#   echo ${red}"$DIFF" ${reset};
+#   echo ${yellow} "-------------" ${reset};
+#   echo ${red} "please try => ${yellow}/!\one by one commit/!\\"${rest};
+#   echo ${red} "1- ${bold}${whiteBg}git rebase -i HEAD~${INCONVCOMMIT_COUNT}"${reset};
+#   echo ${red} "2- ${bold}${whiteBg}replace pick by reward on the commit, save & quit" ${reset};
+#   echo ${red} "3- ${bold}${whiteBg}fix the commit message with convientionnal format, save & quit " ${reset};
+#   echo ${red} "${bold}${whiteBg}Et voila !" ${reset};
+#   echo "";
+#   echo ${red} "${bold}<-- don't be afraid of rebase we believe in you -->";
+#   exit 1;
+# else
+#   echo ${green} ${bold} "all commit are conventionnals" ${reset};
+#   echo ${blue} "-------------" ${reset};
+#   echo ${green} "$CONVCOMMIT_LIST" ${reset};
+#   echo ${blue} "-------------" ${reset};
+#   echo ${bold} ${blue} "good job !" ${reset}
+#   exit 0;
+# fi
