6aeca922918477d6736e23daf2822bc55ec740e0
Mohammed <technocraft.devops@gmail.com>
2022-01-15 21:29:04 +0100

    fix(diffcount): fixing diff count

--- CiCommitLinter.sh
+++ CiCommitLinter.sh
@@ -2,14 +2,13 @@
 
 # Data commits
 CONVCOMMIT_COUNT=$(git log -s --format=%s master..$1  | grep --color=never -P '^(build|ci|feat|fix|refactor|style|docs|test)?\([a-z]*\): (.{1,50}$)' | wc -l);
-INCONVCOMMIT_COUNT=$(git log -s --format=%s master..$1  | wc -l);
+ALLCOMMIT_COUNT=$(git log -s --format=%s master..$1  | wc -l);
 
 CONVCOMMIT_LIST=$(git log -s --format=%s master..$1  | grep -U --color=never -P '^(feat|fix|refactor|test)?\([a-z]*\): (.{1,50}$)');
-INCONVCOMMIT_LIST=$(git log -s --format=%s master..$1 );
+ALLCOMMIT_LIST=$(git log -s --format=%s master..$1 );
 
-DIFF=$(diff  <(echo -e "$INCONVCOMMIT_LIST") <(echo -e "$CONVCOMMIT_LIST"));
-DIFF_COUNT= $(diff  <(echo -e "$INCONVCOMMIT_LIST") <(echo -e "$CONVCOMMIT_LIST") | wc -l);
-
+DIFF=$(diff  <(echo -e "$ALLCOMMIT_LIST"\n) <(echo -e "$CONVCOMMIT_LIST"\n));
+DIFF_COUNT=$(($ALLCOMMIT_COUNT - $CONVCOMMIT_COUNT))
 # Styles
 red=`tput setaf 1`;
 green=`tput setaf 2`;
@@ -25,24 +24,24 @@ reset=`tput sgr0`;
 echo ${DIFF_COUNT} 
 echo ${DIFF} 
 
-# if [ ${DIFF_COUNT} -ge 1 ];then
-#   echo ${underline}${red}  "${bold}not conventionnal commit found" ${reset};
-#   echo ${yellow} "-------------" ${reset};
-#   echo ${red}"$DIFF" ${reset};
-#   echo ${yellow} "-------------" ${reset};
-#   echo ${red} "please try => ${yellow}/!\one by one commit/!\\"${rest};
-#   echo ${red} "1- ${bold}${whiteBg}git rebase -i HEAD~${INCONVCOMMIT_COUNT}"${reset};
-#   echo ${red} "2- ${bold}${whiteBg}replace pick by reward on the commit, save & quit" ${reset};
-#   echo ${red} "3- ${bold}${whiteBg}fix the commit message with convientionnal format, save & quit " ${reset};
-#   echo ${red} "${bold}${whiteBg}Et voila !" ${reset};
-#   echo "";
-#   echo ${red} "${bold}<-- don't be afraid of rebase we believe in you -->";
-#   exit 1;
-# else
-#   echo ${green} ${bold} "all commit are conventionnals" ${reset};
-#   echo ${blue} "-------------" ${reset};
-#   echo ${green} "$CONVCOMMIT_LIST" ${reset};
-#   echo ${blue} "-------------" ${reset};
-#   echo ${bold} ${blue} "good job !" ${reset}
-#   exit 0;
-# fi
+if [ ${DIFF_COUNT} -ge 1 ];then
+  echo ${underline}${red}  "${bold} ${DIFF_COUNT} no-conventionnal commit found" ${reset};
+  echo ${yellow} "-------------" ${reset};
+  echo ${red}"$DIFF" ${reset};
+  echo ${yellow} "-------------" ${reset};
+  echo ${red} "please try => ${yellow}/!\one by one commit/!\\"${rest};
+  echo ${red} "1- ${bold}${whiteBg}git rebase -i HEAD~${ALLCOMMIT_COUNT}"${reset};
+  echo ${red} "2- ${bold}${whiteBg}replace pick by reward on the commit, save & quit" ${reset};
+  echo ${red} "3- ${bold}${whiteBg}fix the commit message with convientionnal format, save & quit " ${reset};
+  echo ${red} "${bold}${whiteBg}Et voila !" ${reset};
+  echo "";
+  echo ${red} "${bold}<-- don't be afraid of rebase we believe in you -->";
+  exit 1;
+else
+  echo ${green} ${bold} "all commit are conventionnals" ${reset};
+  echo ${blue} "-------------" ${reset};
+  echo ${green} "$CONVCOMMIT_LIST" ${reset};
+  echo ${blue} "-------------" ${reset};
+  echo ${bold} ${blue} "good job !" ${reset}
+  exit 0;
+fi
